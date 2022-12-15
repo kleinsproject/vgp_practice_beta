f9a9fd5d6762394da3a52fb63f723fe4a9a3a939
arfes0e2b3c <webyata@gmail.com>
2022-12-15 23:20:29 +0900

    [update] エリア機能による関数の追加、スタイルの変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    <GameField ref="child" />
+    <div class="area-container">
+      <GameField
+        v-for="area in areas"
+        ref="child"
+        :key="area.id"
+        :area-num="area.id"
+        class="area"
+        :class="{ selected: area.selected }"
+        @enable-transition="enableTransition"
+        @disable-transition="disableTransition"
+        @click.native="selected(area)"
+      />
+    </div>
     <GameCommandPalette @doCommand="doCommand" />
     <GameSidebar />
   </div>
@@ -11,14 +23,66 @@ export default {
   name: 'IndexPage',
   methods: {
     doCommand(functionName) {
-      this.$refs.child[functionName]()
+      for (const id of this.selectedAreaIds) {
+        this.$refs.child[id - 1][functionName]()
+      }
     },
+    enableTransition() {
+      for (let i = 0; i < this.areas.length; i++) {
+        this.$refs.child[i].enableTransition()
+      }
+    },
+    disableTransition() {
+      for (let i = 0; i < this.areas.length; i++) {
+        this.$refs.child[i].disableTransition()
+      }
+    },
+    selected(area) {
+      const id = area.id
+      if (!this.selectedAreaIds.includes(id)) {
+        this.selectedAreaIds.push(id)
+        area.selected = true
+      } else {
+        this.selectedAreaIds = this.selectedAreaIds.filter(
+          (cardId) => cardId !== id
+        )
+        area.selected = false
+      }
+    },
+  },
+  data() {
+    return {
+      isTransition: true,
+      areas: [
+        { id: 1, selected: false },
+        { id: 2, selected: false },
+        { id: 3, selected: false },
+        { id: 4, selected: false },
+      ],
+      selectedAreaIds: [],
+    }
   },
 }
 </script>
 
 <style>
-div {
+.area-container {
+  position: absolute;
+  top: 0;
+  left: 260px;
   display: flex;
+  flex-wrap: wrap;
+}
+.area {
+  flex-wrap: nowrap;
+  width: calc((100% - 260px) / 2);
+  height: calc(40vh - 90px);
+  margin: 30px;
+  overflow-x: scroll;
+  transition: 0.3s;
+}
+
+.selected {
+  box-shadow: 0 0 2px 3px rgba(255, 100, 100, 0.3);
 }
 </style>
